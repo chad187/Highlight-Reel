87ad06d0a4bc3a86408ecceea7d5e91bb731e1ea
Chad C <chad187@gmail.com>
2017-04-26 11:38:35 -0700

    i remade because of mis spelling

--- Application/src/main/java/com/example/android/camera2video/VoiceCommand.java
+++ Application/src/main/java/com/example/android/camera2video/VoiceCommand.java
@@ -12,12 +12,12 @@ import android.widget.TextView;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link VoiceCommant.OnFragmentInteractionListener} interface
+ * {@link VoiceCommand.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link VoiceCommant#newInstance} factory method to
+ * Use the {@link VoiceCommand#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class VoiceCommant extends Fragment {
+public class VoiceCommand extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class VoiceCommant extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public VoiceCommant() {
+    public VoiceCommand() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class VoiceCommant extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment VoiceCommant.
+     * @return A new instance of fragment VoiceCommand.
      */
     // TODO: Rename and change types and number of parameters
-    public static VoiceCommant newInstance(String param1, String param2) {
-        VoiceCommant fragment = new VoiceCommant();
+    public static VoiceCommand newInstance(String param1, String param2) {
+        VoiceCommand fragment = new VoiceCommand();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
